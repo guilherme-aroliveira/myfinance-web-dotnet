72f844cd92fa2fd5b6123af566548cea8e5cd500
Guilherme Oliveira <oliveira.g.ar@proton.me>
2024-06-19 11:57:12 -0300

    add entities and EntityFramework

---
+++ 4-) myfinance-web-dotnet-infra/MyFinanceDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using myfinance_web_dotnet_domain.Entities;
+
+namespace myfinance_web_dotnet_infra;
+
+public class MyFinanceDbContext : DbContext
+{
+    // Mapear tabelas do DB com uma entidade de Dominio
+    public DbSet<PlanoConta> PlanoConta { get; set; }
+    public DbSet<Transacao> Transacao { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=myServerAddress;Database=myfinance;Trusted_Connection=True;");
+    }
+}
